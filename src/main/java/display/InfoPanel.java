d1b62de918cfdc3e2389dc25c3122104b7cf4591
Wojciech Ptak <ptakwojt@gmail.com>
2023-05-23 01:48:42 +0200

    slider

--- src/main/java/display/InfoPanel.java
+++ src/main/java/display/InfoPanel.java
@@ -5,15 +5,21 @@ import sim.Sim;
 import sim.SimState;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class InfoPanel extends JPanel implements ActionListener {
+
+public class InfoPanel extends JPanel implements ActionListener, ChangeListener{
     Sim sim;
     SimState simState;
     Dimension dimension;
     JButton startButton, stopButton, setButton;
+    JLabel labelPeople, labelZombie;
+    JSlider sliderPeople,sliderZombie;
 
     public InfoPanel (Sim sim, Input input, SimState simState) {
         this.sim = sim;
@@ -47,6 +53,42 @@ public class InfoPanel extends JPanel implements ActionListener {
         setButton.setText("Set");
         setButton.setEnabled(true);
         add(setButton);
+        
+        labelPeople = new JLabel();
+        labelPeople.setVisible(true);
+        labelPeople.setText("Number of survivors");
+        labelPeople.setBounds(10, 200,220,50);
+        labelPeople.setFont(new Font("Arial",Font.BOLD,15));
+        add(labelPeople);
+        
+        sliderPeople = new JSlider(0,100,25);
+        sliderPeople.setVisible(true);
+        sliderPeople.setBounds(10, 240, 200, 50);
+        sliderPeople.setPaintTicks(true);
+        sliderPeople.setBackground(Color.gray);
+        sliderPeople.setPaintTrack(true);
+        sliderPeople.setMajorTickSpacing(20);
+        sliderPeople.setPaintLabels(true);
+        sliderPeople.addChangeListener(this);
+        add(sliderPeople);
+        
+//        labelZombie = new JLabel();
+//        labelZombie.setVisible(true);
+//        labelZombie.setText("Number of zombies");
+//        labelZombie.setBounds(10, 300,160,50);
+//        labelZombie.setFont(new Font("Arial",Font.BOLD,15));
+//        add(labelZombie);
+//        
+//        sliderZombie = new JSlider();
+//        sliderZombie.setVisible(true);
+//        sliderZombie.setBounds(10, 340, 200, 50);
+//        sliderZombie.setPaintTicks(true);
+//        sliderZombie.setBackground(Color.gray);
+//        sliderZombie.setPaintTrack(true);
+//        sliderZombie.setMajorTickSpacing(20);
+//        sliderZombie.setPaintLabels(true);
+//        add(sliderZombie);
+//        
     }
 
 //    public void paintComponent (Graphics graphics) {
@@ -57,6 +99,13 @@ public class InfoPanel extends JPanel implements ActionListener {
 //
 //        graphics2D.dispose();
 //    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+    	simState.setNumberOfHumans(sliderPeople.getValue());
+    	labelPeople.setText("Number of survivors - "+sliderPeople.getValue());
+    	    
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -83,4 +132,5 @@ public class InfoPanel extends JPanel implements ActionListener {
             startButton.setEnabled(true);
         }
     }
+
 }
